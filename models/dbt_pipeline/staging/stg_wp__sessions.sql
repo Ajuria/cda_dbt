5b77bde0e94c2e55d2a5d2b1653e42607c9e152c
Ajuria <32708980+Ajuria@users.noreply.github.com>
2025-03-31 16:11:32 +0200

    Update stg_wp__sessions.sql

--- models/dbt_pipeline/staging/stg_wp__sessions.sql
+++ models/dbt_pipeline/staging/stg_wp__sessions.sql
@@ -1,12 +1,12 @@
 SELECT
-    ID                          AS session_id,
-    post_title                  AS session_label,
-    post_name                   AS session_slug,
-    post_type,
-    post_status,
-    post_date,
-    post_modified,
-    post_parent,
-    post_author                 AS user_id
-FROM {{ source('wordpress', 'posts') }}
-WHERE post_type = 'session'
+    session_id,
+    source_platform,
+    session_source_url,
+    user_id,
+    brand_id,
+    device,
+    geo_ip,
+    geo_name,
+    started_at,
+    ended_at
+FROM `cda-database.cda_owned.session`
